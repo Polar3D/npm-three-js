00b78143501f5b10aa89a2306107dee5b3269141
Marcus Zeng <ifenges@gmail.com>
2017-09-13 11:12:39 -0400

    Update TransformControls.js

--- addons/TransformControls.js
+++ addons/TransformControls.js
@@ -233,13 +233,13 @@ module.exports = function (THREE) {
     arrowGeometry.merge( mesh.geometry, mesh.matrix );
 
     var lineXGeometry = new THREE.BufferGeometry();
-    lineXGeometry.addAttribute( 'position', new THREE.Float32Attribute( [ 0, 0, 0,  1, 0, 0 ], 3 ) );
+    lineXGeometry.addAttribute( 'position', new THREE.Float32BufferAttribute( [ 0, 0, 0,  1, 0, 0 ], 3 ) );
 
     var lineYGeometry = new THREE.BufferGeometry();
-    lineYGeometry.addAttribute( 'position', new THREE.Float32Attribute( [ 0, 0, 0,  0, 1, 0 ], 3 ) );
+    lineYGeometry.addAttribute( 'position', new THREE.Float32BufferAttribute( [ 0, 0, 0,  0, 1, 0 ], 3 ) );
 
     var lineZGeometry = new THREE.BufferGeometry();
-    lineZGeometry.addAttribute( 'position', new THREE.Float32Attribute( [ 0, 0, 0,  0, 0, 1 ], 3 ) );
+    lineZGeometry.addAttribute( 'position', new THREE.Float32BufferAttribute( [ 0, 0, 0,  0, 0, 1 ], 3 ) );
 
     this.handleGizmos = {
 
@@ -370,7 +370,7 @@ module.exports = function (THREE) {
         if ( facing === 'z' ) vertices.push( Math.sin( i / 32 * Math.PI ) * radius, Math.cos( i / 32 * Math.PI ) * radius, 0 );
 
       }
-      geometry.addAttribute( 'position', new THREE.Float32Attribute( vertices, 3 ) );
+      geometry.addAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
       return geometry;
 
     };
@@ -504,13 +504,13 @@ module.exports = function (THREE) {
     arrowGeometry.merge( mesh.geometry, mesh.matrix );
 
     var lineXGeometry = new THREE.BufferGeometry();
-    lineXGeometry.addAttribute( 'position', new THREE.Float32Attribute( [ 0, 0, 0,  1, 0, 0 ], 3 ) );
+    lineXGeometry.addAttribute( 'position', new THREE.Float32BufferAttribute( [ 0, 0, 0,  1, 0, 0 ], 3 ) );
 
     var lineYGeometry = new THREE.BufferGeometry();
-    lineYGeometry.addAttribute( 'position', new THREE.Float32Attribute( [ 0, 0, 0,  0, 1, 0 ], 3 ) );
+    lineYGeometry.addAttribute( 'position', new THREE.Float32BufferAttribute( [ 0, 0, 0,  0, 1, 0 ], 3 ) );
 
     var lineZGeometry = new THREE.BufferGeometry();
-    lineZGeometry.addAttribute( 'position', new THREE.Float32Attribute( [ 0, 0, 0,  0, 0, 1 ], 3 ) );
+    lineZGeometry.addAttribute( 'position', new THREE.Float32BufferAttribute( [ 0, 0, 0,  0, 0, 1 ], 3 ) );
 
     this.handleGizmos = {
 
